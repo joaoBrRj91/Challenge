9ad9eb9f16cc00a7e936d3738a3746c79d2ca518
João Nascimento <joao1991algoritmo@gmail.com>
2018-02-18 14:11:58 -0300

    Criação da camada de infra e codificação para consumo do web service do github

---
+++ ChallengeListRepositories/ConfiguracoesProjetos/ConfigConsumoGitWebServices.cs
@@ -0,0 +1,49 @@
+﻿namespace ConfiguracoesProjetos
+{
+    public static class ConfigConsumoGitWebServices
+    {
+
+        #region Configuração, das constantes, de acesso ao web service do github
+        public const string ENDERECO_BASE = "https://api.github.com";
+
+        public const string HEADER_API_GIT = "application/vnd.github.v3+json";
+
+        #endregion
+
+        #region Propriedades
+        private static string usuarioGit = string.Empty;
+
+        public static string UsuarioGit
+        {
+            get
+            {
+                return usuarioGit;
+            }
+            set
+            {
+                UsuarioGit = value;
+                uriUsuarioRepositorios = string.Empty;
+            }
+        }
+
+        private static string uriUsuarioRepositorios = string.Empty;
+
+        public  static string UriUsuarioRepositorios
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(uriUsuarioRepositorios))
+                    uriUsuarioRepositorios = ObterUriRepositoriosUsuario();
+
+                return uriUsuarioRepositorios;
+            }
+            
+        }
+        #endregion
+
+        #region Metodos auxiliares
+        private static string ObterUriRepositoriosUsuario() => string.Format("/{0}/{1}", UsuarioGit, "repos");
+        #endregion
+
+    }
+}
---
+++ ChallengeListRepositories/ConsumoGitWebService/GitWebService.cs
@@ -0,0 +1,27 @@
+﻿
+using ConfiguracoesProjetos;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace ConsumoGitWebService
+{
+    public static class GitWebService
+    {
+
+        public  static void ListarRepositoriosPorUsuario()
+        {
+            HttpClient httpCliente = new HttpClient();
+            httpCliente.BaseAddress = new Uri(ConfigConsumoGitWebServices.ENDERECO_BASE);
+            httpCliente.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(ConfigConsumoGitWebServices.HEADER_API_GIT));
+            HttpResponseMessage response =  httpCliente.GetAsync(ConfigConsumoGitWebServices.UriUsuarioRepositorios).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var repositoriosJsonString = response.Content.ReadAsStringAsync();
+
+            }
+
+        }
+    }
+}
